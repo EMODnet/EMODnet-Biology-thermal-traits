558bd161c6652282a650bd48af10fe14e6959933
tomjwebb <t.j.webb@sheffield.ac.uk>
2018-11-09 16:24:33 +0000

    Update do_gridded_t_affin_plot.R

--- R/do_gridded_t_affin_plot.R
+++ R/do_gridded_t_affin_plot.R
@@ -102,5 +102,6 @@ do_gridded_t_affin_plot <- function(fgrp,
   # print and/or save plot if requested
   if(print_plot == TRUE){print(t_plot)}
   if(save_plot == TRUE){
+    plot_tit <- plot_tit %>% str_replace_all(c(" ", ","), "")
     ggsave(filename = paste0(plot_tit, ".png"), plot = t_plot)}	
 }
